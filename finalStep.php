b00dbfd64cb63d6a8822d89c7c06237c2edd7a07
B00143361 Amos Fakrogha <B00143361@mytudublin.ie>
2023-11-22 10:04:33 +0000

    part2

--- finalStep.php
+++ finalStep.php
@@ -1,42 +1,23 @@
 <?php
 include("finalStepHeader.html");
 
-//translate the comments below into PHP code underneath each comment
 
-//start a session
+session_start();
 
-//echo the passenger's firstname from the appropriate session variable
 
-echo "<BR>";
+echo "Passenger's First Name: " . $_SESSION['passengerFN'] . "<br>";
 
-//echo the passenger's surname from the appropriate session variable
 
-echo "<BR>";
+echo "Passenger's Surname: " . $_SESSION['passengerSN'] . "<br>";
 
-// if the luggage session variable is on
 
-    //echo the amount of bags under ten kilos the passenger is bringing
-    
-    echo "<BR>";
-    
-    //echo the amount of bags over ten kilos the passenger is bringing
-    
-//end if block
+if (isset($_SESSION['luggage'])) {
+  
+    echo "Number of Bags Under 10KG: " . $_SESSION['subTenKG'] . "<br>";
+
+    echo "Number of Bags 10-20KG: " . $_SESSION['overTenKG'] . "<br>";
+}
+
 
 ?>
-</h4></div></div>
-<form method="POST" action="confirm.php">
-    <div class="form-group">        
-        <label class="control-label col-sm-12 text-center">Is the above information correct?</label>
-        <div class="checkbox col-sm-12 text-center">
-          <label><input type="checkbox" name="confirm">Yes</input>
-        </div>
-    </div>
-   
-        
-      <div class="col-sm-12 text-center">
-        <button type="submit" class="btn btn-default">Continue</button>
-      </div>
-    </div>
-    
-</form>
+
