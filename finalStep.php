5584dfed7419bd5c3321d4926116807150b77841
B00143361 Amos Fakrogha <B00143361@mytudublin.ie>
2023-11-22 10:16:04 +0000

    part3

--- finalStep.php
+++ finalStep.php
@@ -4,15 +4,13 @@ include("finalStepHeader.html");
 
 session_start();
 
-
 echo "Passenger's First Name: " . $_SESSION['passengerFN'] . "<br>";
-
 
 echo "Passenger's Surname: " . $_SESSION['passengerSN'] . "<br>";
 
 
 if (isset($_SESSION['luggage'])) {
-  
+    
     echo "Number of Bags Under 10KG: " . $_SESSION['subTenKG'] . "<br>";
 
     echo "Number of Bags 10-20KG: " . $_SESSION['overTenKG'] . "<br>";
@@ -20,4 +18,15 @@ if (isset($_SESSION['luggage'])) {
 
 
 ?>
-
+<form method="POST" action="confirm.php">
+    <div class="form-group">        
+        <label class="control-label col-sm-12 text-center">Is the above information correct?</label>
+        <div class="checkbox col-sm-12 text-center">
+            <label><input type="checkbox" name="confirm">Yes</label>
+        </div>
+    </div>
+   
+    <div class="col-sm-12 text-center">
+        <button type="submit" class="btn btn-default">Continue</button>
+    </div>
+</form>
